bebfcddfefc2f893d515de407bfaea1afe39a1d4
RJVB <rjvbertin@gmail.com>
2013-09-30 11:38:40 +0200

    tweaks

--- setup-macstl.py
+++ setup-macstl.py
@@ -1,5 +1,9 @@
 from distutils.core import setup, Extension
-from numpy import get_include as numpy_get_include
+try:
+	from numpy import get_include as numpy_get_include
+	has_numpy = True
+except:
+	has_numpy = False
 
 try:
 	from distutils.command.build_py import build_py_2to3 as build_py
@@ -28,32 +32,33 @@ def doall():
 		 py_modules=['DataFrame'], cmdclass={'build_py':build_py} )
 
 # these depend on numpy:
-	setup(name='rjvbFilt', version='1.0',
-		 description = 'a package providing some filtring functions',
-		 ext_modules = [Extension('rjvbFilt',
-							 sources=['rjvbFilt.c','rjvbFilters.cpp'],
-							 depends=['ParsedSequence.h','rjvbFilters.c','rjvbFilters.h'],
-							 include_dirs = [numpy_get_include()],
-							 extra_compile_args =['-g','-msse2','-faltivec']
-							 )])
+	if has_numpy:
+		setup(name='rjvbFilt', version='1.0',
+			 description = 'a package providing some filtring functions',
+			 ext_modules = [Extension('rjvbFilt',
+								 sources=['rjvbFilt.c','rjvbFilters.cpp'],
+								 depends=['ParsedSequence.h','rjvbFilters.c','rjvbFilters.h'],
+								 include_dirs = [numpy_get_include()],
+								 extra_compile_args =['-g','-msse2','-faltivec']
+								 )])
 
-	setup(name='sincos', version='1.0',
-		 description = 'a package providing a sincos function',
-		 ext_modules = [Extension('sincos',
-							 sources=['sincos.c','mips_sincos.c','cephes_sincos.c'],
-							 depends=['sse_mathfun/sse_mathfun.h'],
-							 include_dirs = [numpy_get_include()],
-							 extra_compile_args =['-g','-msse2','-faltivec']
-							 )])
+		setup(name='sincos', version='1.0',
+			 description = 'a package providing a sincos function',
+			 ext_modules = [Extension('sincos',
+								 sources=['sincos.c','mips_sincos.c','cephes_sincos.c'],
+								 depends=['sse_mathfun/sse_mathfun.h'],
+								 include_dirs = [numpy_get_include()],
+								 extra_compile_args =['-g','-msse2','-faltivec']
+								 )])
 
-	setup(name='fmadd', version='1.0',
-		 description = 'a package providing an fmadd function',
-		 ext_modules = [Extension('fmadd',
-							 sources=['fmadd.cpp'],
-							 depends=['fmadd.c'],
-							 extra_compile_args =['-g','-msse2','-faltivec'],
-							 include_dirs = [numpy_get_include()]
-							 )])
+		setup(name='fmadd', version='1.0',
+			 description = 'a package providing an fmadd function',
+			 ext_modules = [Extension('fmadd',
+								 sources=['fmadd.cpp'],
+								 depends=['fmadd.c'],
+								 extra_compile_args =['-g','-msse2','-faltivec'],
+								 include_dirs = [numpy_get_include()]
+								 )])
 
 doall()
 
